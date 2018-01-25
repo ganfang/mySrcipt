a99045eabe0cb378f335e8b54bdbc191e3ff1b92
ganfang <923498319@qq.com>
2018-01-25 15:46:58 +0800

    Update 11.js

--- 11.js
+++ 11.js
@@ -1,1 +1,31 @@
-哈哈
+
+function ChangeArrIndex(){
+var arrIndex=localStorage.getItem('arrindex');
+var cookieArr=GetCookieArr();
+ var arrLength=cookieArr.length;
+ arrIndex=(arrIndex+1)%arrLength;
+  localStorage.setItem ('arrindex',arrIndex);
+}
+
+ function changeCookie(){
+   var arrIndex=localStorage.getItem('arrindex');
+   var cookieArr=GetCookieArr();
+	 var cookieStr=cookieArr[arrIndex];
+    setCookie("MydlCookie",cookieStr);
+    //document.getElementById("form1").submit();
+ }
+function setCookie(name,value)
+{
+    var Days = 30*12*100;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days*24*60*60*1000);
+    document.cookie = name + "="+value + ";expires=" + exp.toGMTString();
+}
+function GetCookieArr(){
+  var cookieStr=localStorage.getItem('cookieArr');
+  if(!cookieStr){
+    return [];
+  }
+  var cookieArr=cookieStr.split(",");
+  return cookieArr;
+}
